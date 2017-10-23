46ea58481baa60652a9e11ae86b0b82cd556f400
Dan Mopsick <dmop497@gmail.com>
2017-10-23 15:20:57 -0400

    Exploro interface pulls in slightly more information now.

--- application/controllers/exploro.php
+++ application/controllers/exploro.php
@@ -7,25 +7,25 @@ class exploro extends CI_Controller
 	}
 
    public function index()
-    {
-       $date = date_default_timezone_set('US/Eastern');
-       $this->load->view('search');
-    }
+   {
+     $date = date_default_timezone_set('US/Eastern');
+     $this->load->view('search');
+   }
 
 	public function searchKeyWords()
-	 {
-      	$q = $this -> input -> get('q');
+  {
+  	$q = $this -> input -> get('q');
 		$q = str_replace(" ","%20", $q);
-		$resultsLink = "http://35.162.165.138:8983/solr/exploro-test/query?q=" . $q."&facet=true&facet.field=collection&facet.field=unitdate&facet.field=category&facet.field=format&facet.field=physdesc&facet.field=location";
+		$resultsLink = "http://35.162.165.138:8983/solr/exploro/query?q=" . $q."&facet=true&facet.field=collection&facet.field=datesingle&facet.field=category";
  		$json = file_get_contents($resultsLink);
-     	$data['results'] = json_decode($json);
+   	$data['results'] = json_decode($json);
 
 		$dplaResultsLink = "http://api.dp.la/v2/items?q=" . $q ."&page_size=13&facets=sourceResource.subject.name&api_key=96410fe9eab08488c9a3da4e9641669f";
 	 	$json1 = file_get_contents($dplaResultsLink);
-     	$data['dplaResults'] = json_decode($json1);
+   	$data['dplaResults'] = json_decode($json1);
 
 	 	$this->load->view('results', $data);
-     }
+  }
 
 	 public function searchSubjects()
 	 {
