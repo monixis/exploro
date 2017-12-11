8bb18d509f538910e4180a3af085874871cf9a0e
Dan Mopsick <dmop497@gmail.com>
2017-12-11 14:24:03 -0500

    Fixed issue with dashes in facet name by using javascript lastIndexOf()

--- application/views/results.php
+++ application/views/results.php
@@ -279,12 +279,9 @@ div.tab button.active {
 
     $('a.tags').click(function(){
       var searchTerm = $('input#searchBox').val();
-      var selectedTag = ($(this).parents('div.panel').attr('id')) + ' : ' + ($(this).text().substr(0, $(this).text().indexOf('-')));
-      // A very very hacky fix to the non digitized issue.. it's because there is a `-` in the facet
+      var selectedTag = ($(this).parents('div.panel').attr('id')) + ' : ' + ($(this).text().substr(0, $(this).text().lastIndexOf('-')));
+      
       // alert(selectedTag);
-      if (selectedTag == "category : Non") {
-        selectedTag = "category: Non-Digitized";
-      }
       $('#selectedFacet').append('<button class="taglist" style="border: 1px solid #cccccc; background: #eeeeee; padding: 5px; margin-right: 10px; margin-top: 5px;">'+ selectedTag +'<a href="#" class="remove" id="'+ selectedTag +'" style="margin-left:10px;"> X </a></button>');
       $('input#queryTag').val($('input#queryTag').val() + "fq=" + selectedTag);
       var queryTag = $('input#queryTag').val();
